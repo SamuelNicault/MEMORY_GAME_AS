61fd08247f56fc9cb4e78a6fcf4f50596ad25c8b
SamuelNicault <59608515+SamuelNicault@users.noreply.github.com>
2020-05-12 15:59:48 +0200

    Update Scene_Jeu.js

--- Scenes/Scene_Jeu.js
+++ Scenes/Scene_Jeu.js
@@ -19,15 +19,31 @@ class SceneJeu extends Phaser.Scene {
     this.load.image('roiR2','assets/cardHeartsK.png');
     this.load.image('reineR','assets/cardDiamondsQ.png');
     this.load.image('reineN2','assets/cardHeartsQ.png');
-
-
-  }
-
-    this.load.image('back','assets/back.png');
+    this.load.image('carte','assets/cardBack_green5.png');
+    this.load.image('back','assets/back.jpg');
   }//End preload
 
 
   create(){
+    this.add.image(0,0,'back').setOrigin(0,0);
+
+    this.button=this.add.sprite(150, 100,'valetN'); //à remplacer par la mniature de votre image
+    this.button.setAlpha(0);
+
+    this.pass=this.add.sprite(150, 100,'carte');
+    this.pass.setInteractive();
+    this.pass.alwaysEnabled = true;
+
+
+    this.pass.on('pointerdown',() => {
+      this.button.setAlpha(1);
+      this.pass.setAlpha(0);
+    })
+
+    this.pass.on('pointerup',() => {
+      this.pass.setAlpha(1);
+      this.button.setAlpha(0);
+    });
   }//End create
 
   update() {
