38c017d7708cd485105524e497608248ae74dfcf
SamuelNicault <59608515+SamuelNicault@users.noreply.github.com>
2020-05-12 17:10:39 +0200

    Update Scene_Jeu.js

--- Scenes/Scene_Jeu.js
+++ Scenes/Scene_Jeu.js
@@ -30,16 +30,22 @@ class SceneJeu extends Phaser.Scene {
 
     this.button=this.add.sprite(70, 200,'valetN'); //à remplacer par la mniature de votre image
     this.button.setAlpha(0);
+    this.button.valeur=0;
     this.button2=this.add.sprite(210, 200,'valetN2'); //à remplacer par la mniature de votre image
     this.button2.setAlpha(0);
+    this.button2.valeur=0;
     this.button3=this.add.sprite(340, 200,'reineN2'); //à remplacer par la mniature de votre image
     this.button3.setAlpha(0);
+    this.button3.valeur=0;
     this.button4=this.add.sprite(470, 200,'roiN'); //à remplacer par la mniature de votre image
     this.button4.setAlpha(0);
+    this.button4.valeur=0;
     this.button5=this.add.sprite(600, 200,'reineN2'); //à remplacer par la mniature de votre image
     this.button5.setAlpha(0);
+    this.button5.valeur=0;
     this.button6=this.add.sprite(730, 200,'roiN2'); //à remplacer par la mniature de votre image
     this.button6.setAlpha(0);
+    this.button6.valeur=0;
 
     this.pass=this.add.sprite(70, 200,'carte');
     this.pass2=this.add.sprite(210, 200,'carte');
@@ -62,63 +68,87 @@ class SceneJeu extends Phaser.Scene {
 
     this.pass.on('pointerdown',() => {
       this.button.setAlpha(1);
+      this.button.valeur==1;
       this.pass.setAlpha(0);
+      if(this.button2.valeur==0){
+        this.timedEvent = this.time.delayedCall(5000, passReac, [], this);
+      }
     })
 
-    this.pass.on('pointerup',() => {
+    function passReac(){
       this.pass.setAlpha(1);
       this.button.setAlpha(0);
-    });
+    }
 
     this.pass2.on('pointerdown',() => {
+      this.button2.valeur = 1;
       this.button2.setAlpha(1);
       this.pass2.setAlpha(0);
+      if(this.button1.valeur==0){
+        this.timedEvent = this.time.delayedCall(5000, passReac2, [], this);
+      }
     })
 
-    this.pass2.on('pointerup',() => {
+    function passReac2(){
       this.pass2.setAlpha(1);
       this.button2.setAlpha(0);
-    });
+    }
 
     this.pass3.on('pointerdown',() => {
+      this.button3.valeur = 1;
       this.button3.setAlpha(1);
       this.pass3.setAlpha(0);
+      if(this.button5.valeur==0){
+        this.timedEvent = this.time.delayedCall(5000, passReac3, [], this);
+      }
     })
 
-    this.pass3.on('pointerup',() => {
+    function passReac3(){
       this.pass3.setAlpha(1);
       this.button3.setAlpha(0);
-    });
+    }
 
     this.pass4.on('pointerdown',() => {
+      this.button4.valeur = 1;
       this.button4.setAlpha(1);
       this.pass4.setAlpha(0);
+      if(this.button6.valeur==0){
+        this.timedEvent = this.time.delayedCall(5000, passReac4, [], this);
+      }
     })
 
-    this.pass4.on('pointerup',() => {
+   function passReac4(){
       this.pass4.setAlpha(1);
       this.button4.setAlpha(0);
-    });
+    }
 
     this.pass5.on('pointerdown',() => {
+      this.button5.valeur = 1;
       this.button5.setAlpha(1);
       this.pass5.setAlpha(0);
+      if(this.button3.valeur==0){
+        this.timedEvent = this.time.delayedCall(5000, passReac5, [], this);
+      }
     })
 
-    this.pass5.on('pointerup',() => {
+    function passReac5(){
       this.pass5.setAlpha(1);
       this.button5.setAlpha(0);
-    });
+    }
 
     this.pass6.on('pointerdown',() => {
+      this.button6.valeur = 1;
       this.button6.setAlpha(1);
       this.pass6.setAlpha(0);
+      if(this.button4.valeur==0){
+        this.timedEvent = this.time.delayedCall(5000, passReac6, [], this);
+      }
     })
 
-    this.pass6.on('pointerup',() => {
+    function passReac(){
       this.pass6.setAlpha(1);
       this.button6.setAlpha(0);
-    });
+    }
   }//End create
 
   update() {
