6e138fc0ffe52d813b9b9dbf5dd3f868ef477d9b
SamuelNicault <59608515+SamuelNicault@users.noreply.github.com>
2020-05-12 16:42:54 +0200

    Update Scene_Jeu.js

--- Scenes/Scene_Jeu.js
+++ Scenes/Scene_Jeu.js
@@ -25,15 +25,40 @@ class SceneJeu extends Phaser.Scene {
 
 
   create(){
+
     this.add.image(0,0,'back').setOrigin(0,0);
 
-    this.button=this.add.sprite(150, 100,'valetN'); //à remplacer par la mniature de votre image
+    this.button=this.add.sprite(70, 100,'valetN'); //à remplacer par la mniature de votre image
     this.button.setAlpha(0);
+    this.button2=this.add.sprite(210, 100,'valetN'); //à remplacer par la mniature de votre image
+    this.button2.setAlpha(0);
+    this.button3=this.add.sprite(340, 100,'valetN'); //à remplacer par la mniature de votre image
+    this.button3.setAlpha(0);
+    this.button4=this.add.sprite(470, 100,'valetN'); //à remplacer par la mniature de votre image
+    this.button4.setAlpha(0);
+    this.button5=this.add.sprite(600, 100,'valetN'); //à remplacer par la mniature de votre image
+    this.button5.setAlpha(0);
+    this.button6=this.add.sprite(730, 100,'valetN'); //à remplacer par la mniature de votre image
+    this.button6.setAlpha(0);
 
-    this.pass=this.add.sprite(150, 100,'carte');
+    this.pass=this.add.sprite(70, 100,'carte');
+    this.pass2=this.add.sprite(210, 100,'carte');
+    this.pass3=this.add.sprite(340, 100,'carte');
+    this.pass4=this.add.sprite(470, 100,'carte');
+    this.pass5=this.add.sprite(600, 100,'carte');
+    this.pass6=this.add.sprite(730, 100,'carte');
     this.pass.setInteractive();
     this.pass.alwaysEnabled = true;
-
+    this.pass2.setInteractive();
+    this.pass2.alwaysEnabled = true;
+    this.pass3.setInteractive();
+    this.pass3.alwaysEnabled = true;
+    this.pass4.setInteractive();
+    this.pass4.alwaysEnabled = true;
+    this.pass5.setInteractive();
+    this.pass5.alwaysEnabled = true;
+    this.pass6.setInteractive();
+    this.pass6.alwaysEnabled = true;
 
     this.pass.on('pointerdown',() => {
       this.button.setAlpha(1);
@@ -43,6 +68,56 @@ class SceneJeu extends Phaser.Scene {
     this.pass.on('pointerup',() => {
       this.pass.setAlpha(1);
       this.button.setAlpha(0);
+    });
+
+    this.pass2.on('pointerdown',() => {
+      this.button2.setAlpha(1);
+      this.pass2.setAlpha(0);
+    })
+
+    this.pass2.on('pointerup',() => {
+      this.pass2.setAlpha(1);
+      this.button2.setAlpha(0);
+    });
+
+    this.pass3.on('pointerdown',() => {
+      this.button3.setAlpha(1);
+      this.pass3.setAlpha(0);
+    })
+
+    this.pass3.on('pointerup',() => {
+      this.pass3.setAlpha(1);
+      this.button3.setAlpha(0);
+    });
+
+    this.pass4.on('pointerdown',() => {
+      this.button4.setAlpha(1);
+      this.pass4.setAlpha(0);
+    })
+
+    this.pass4.on('pointerup',() => {
+      this.pass4.setAlpha(1);
+      this.button4.setAlpha(0);
+    });
+
+    this.pass5.on('pointerdown',() => {
+      this.button5.setAlpha(1);
+      this.pass5.setAlpha(0);
+    })
+
+    this.pass5.on('pointerup',() => {
+      this.pass5.setAlpha(1);
+      this.button5.setAlpha(0);
+    });
+
+    this.pass6.on('pointerdown',() => {
+      this.button6.setAlpha(1);
+      this.pass6.setAlpha(0);
+    })
+
+    this.pass6.on('pointerup',() => {
+      this.pass6.setAlpha(1);
+      this.button6.setAlpha(0);
     });
   }//End create
 
