fbeaad2bd8d86070dc43e175cbf0f7777a15fd4c
SamuelNicault <59608515+SamuelNicault@users.noreply.github.com>
2020-05-12 15:23:25 +0200

    Update Scene_Jeu.js

--- Scenes/Scene_Jeu.js
+++ Scenes/Scene_Jeu.js
@@ -7,18 +7,19 @@ class SceneJeu extends Phaser.Scene {
   }//End init
 
   preload(){
-    this.load.image('valetN','assets/monde/cardClubsJ.png');
-    this.load.image('valetN2','assets/monde/cardSpadesJ.png');
-    this.load.image('roiN','assets/monde/cardClubsK.png');
-    this.load.image('roiN2','assets/monde/cardSpadesK.png');
-    this.load.image('reineN','assets/monde/cardClubsQ.png');
-    this.load.image('reineN2','assets/monde/cardSpadesQ.png');
-    this.load.image('valetR','assets/monde/cardDiamondsJ.png');
-    this.load.image('valetR2','assets/monde/cardHeartsJ.png');
-    this.load.image('roiR','assets/monde/cardDiamondsK.png');
-    this.load.image('roiR2','assets/monde/cardHeartsK.png');
-    this.load.image('reineR','assets/monde/cardDiamondsQ.png');
-    this.load.image('reineN2','assets/monde/cardHeartsQ.png');
+    this.load.image('valetN','assets/cardClubsJ.png');
+    this.load.image('valetN2','assets/cardSpadesJ.png');
+    this.load.image('roiN','assets/cardClubsK.png');
+    this.load.image('roiN2','assets/cardSpadesK.png');
+    this.load.image('reineN','assets/cardClubsQ.png');
+    this.load.image('reineN2','assets/cardSpadesQ.png');
+    this.load.image('valetR','assets/cardDiamondsJ.png');
+    this.load.image('valetR2','assets/cardHeartsJ.png');
+    this.load.image('roiR','assets/cardDiamondsK.png');
+    this.load.image('roiR2','assets/cardHeartsK.png');
+    this.load.image('reineR','assets/cardDiamondsQ.png');
+    this.load.image('reineN2','assets/cardHeartsQ.png');
+    this.load.image('back','assets/back.png');
   }//End preload
 
   create(){
