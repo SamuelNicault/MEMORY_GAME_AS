99f5a7761ee970bd20745c8aec378a0467a296c3
SamuelNicault <59608515+SamuelNicault@users.noreply.github.com>
2020-05-12 15:17:49 +0200

    Nettoyage

--- Scenes/Scene_Jeu.js
+++ Scenes/Scene_Jeu.js
@@ -4,46 +4,15 @@ class SceneJeu extends Phaser.Scene {
     }
 
   init(){
-    this.fin;
-  }
+  }//End init
 
   preload(){
-    this.load.image('fin','assets/monde/credits.png');
-  }
+  }//End preload
 
   create(){
-    this.fin = this.physics.add.sprite(512,1500,'fin');
-  	this.fin.body.setGravityY(-300);
-  	this.timedEvent = this.time.delayedCall(33000, changeLevel, [], this);
-    this.keys = this.input.keyboard.addKeys('A');
-
-  	function changeLevel () {
-  		console.log('change de level');
-  		this.scene.start('Titre');
-  	}
-  }
-
+  }//End create
 
   update() {
+  }//End update
 
-    if(this.keys.A.isDown){
-			this.scene.start('Titre');
-		}
-
-
-    if (this.fin.y >= -1800){
-        this.tweens.add({
-          targets: this.fin,
-
-          y : -6000,
-          // alpha: { start: 0, to: 1 },
-          // alpha: 1,
-          // alpha: '+=1',
-          ease: 'Linear',       // 'Cubic', 'Elastic', 'Bounce', 'Back'
-          duration: 100000,
-          repeat: 0,            // -1: infinity
-          yoyo: false
-      });
-    }
-  }
-}
+}//End prog
--- Scenes/scene.js
+++ Scenes/scene.js
@@ -11,7 +11,7 @@ var config = {
 		}
 	},
 
-	scene: [Variables, SceneJeu]
+	scene: [SceneJeu]
 
 };
 
